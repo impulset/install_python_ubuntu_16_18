b8d5b7c099bad44698eb4f947977f4a486440d4e
Pavel <69989483+sizykhpa@users.noreply.github.com>
2021-10-29 14:35:19 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+VER=3.8.9
+MVER=3.8
+
 sudo apt update
 
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
 
-wget https://www.python.org/ftp/python/3.8.9/Python-3.8.9.tgz
+wget https://www.python.org/ftp/python/3.8.9/Python-$VER.tgz
 
-tar -xf Python-3.8.9.tgz
+tar -xf Python-$VER.tgz
 
-cd Python-3.8.9
+cd Python-$VER
 ./configure --enable-optimizations
 
 make -j $(nproc)
@@ -16,6 +19,6 @@ make -j $(nproc)
 sudo make altinstall
 
 sudo rm -f /usr/bin/python3
-sudo ln -s /usr/local/bin/python3.8 /usr/bin/python
+sudo ln -s /usr/local/bin/python$MVER /usr/bin/python
 
-/usr/local/bin/python3.8 -m pip install --upgrade pip
+/usr/local/bin/python$MVER -m pip install --upgrade pip
